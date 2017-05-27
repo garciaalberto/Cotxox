c0d47fe69440a57c659037d7fd35a984b30c09c1
garciaalberto <Bertooooo10@gmail.com>
2017-05-27 16:05:07 +0200

    Skeleton of the application
    Mounted the packages, and the basic logic on them

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/68/90b204561e4200171a3488252ce5c0d7
@@ -0,0 +1,13 @@
+package FooBarSpam.Cotxox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Cotxox/src/main/java/FooBarSpam/Cotxox/Conductor/Conductor.java
@@ -0,0 +1,86 @@
+package FooBarSpam.Cotxox.Conductor;
+
+import java.util.ArrayList;
+
+public class Conductor {
+	// Propiedades privadas
+	private String nombre = null;
+	private String matricula = null;
+	private String modelo = null;
+	private ArrayList<Byte> valoraciones = new ArrayList<Byte>();
+	private boolean ocupado = false;
+	private double valoracionMedia = 0d;
+	
+	// Constructores
+	public Conductor() {
+		
+	}
+	
+	public Conductor(String nombre) {
+		this.nombre = nombre;
+	}
+
+	// Setters y Getters
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public String getMatricula() {
+		return this.matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getModelo() {
+		return this.modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public void setValoracion(Byte valoracion) {
+		this.valoraciones.add(valoracion);
+		this.actualizarMedia();
+	}
+
+	public ArrayList<Byte> getValoraciones() {
+		return this.valoraciones;
+	}
+
+	public boolean isOcupado() {
+		return ocupado;
+	}
+
+	public void setOcupado(boolean ocupado) {
+		this.ocupado = ocupado;
+	}
+	
+	public void actualizarMedia() {
+
+		double suma = 0.0;
+
+		for (Byte valoracion : this.getValoraciones()) {
+			suma += valoracion;
+		}
+		
+		double media = suma / this.getValoraciones().size();
+		this.setValoracionMedia(media);
+	}
+
+	public double getValoracionMedia() {
+		return valoracionMedia;
+	}
+
+	public void setValoracionMedia(double valoracionMedia) {
+		this.valoracionMedia = valoracionMedia;
+	}
+	
+	public double getValoracion() {
+		return this.valoracionMedia;
+	}
+	
+	
+}
