89b79e80addc7580e5f433aa582d97cc36776cfe
garciaalberto <Bertooooo10@gmail.com>
2017-06-01 18:13:07 +0200

    Thymeleaf molt fort

--- Cotxox/src/main/java/FooBarSpam/Cotxox/mvc/CarreraServiceController.java
+++ Cotxox/src/main/java/FooBarSpam/Cotxox/mvc/CarreraServiceController.java
@@ -5,11 +5,9 @@
  */
 package FooBarSpam.Cotxox.mvc;
 
-import FooBarSpam.Cotxox.repository.Carrera;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 /**
  *
  * @author Sergi
@@ -17,24 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class CarreraServiceController {
 
-    /*
-    System.out.println("\n#####" + "\t Set Pickup: \t" + "#####\n" );
-		
-		System.out.println("Visa: " + carrera.getTarjetaCredito());
-		System.out.println("From: "+ carrera.getOrigen());
-		System.out.println("To: " + carrera.getDestino());
-		System.out.println("Distance: " + carrera.getDistancia());
-     */
-    @RequestMapping("/setpickup")
-    public String setPickUp(Carrera carrera) {
-
-        String sms = "\\n#####\" + \"\\t Set Pickup: \\t\" + \"#####\\n";
-        sms.concat("\nVisa: " + carrera.getTarjetaCredito());
-        sms.concat("\nFrom: " + carrera.getOrigen());
-        sms.concat("\nTo:" + carrera.getDestino());
-        sms.concat("\nDistance:" + carrera.getDistancia());
-
-        return "s";
+    @RequestMapping(path="/")
+    public String setPickUp() {
+    	return "SetPickup";
     }
 
 }
