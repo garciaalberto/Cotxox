cf6c53ead0dff63f5fa0e034b26ba869e9b88632
seergi93 <sergiroberti93@gmail.com>
2017-06-02 22:07:38 +0200

    metode amb parametra URL

--- CotxoxApp/src/main/java/org/foobarspam/mvc/CarreraServiceController.java
+++ CotxoxApp/src/main/java/org/foobarspam/mvc/CarreraServiceController.java
@@ -5,19 +5,38 @@
  */
 package org.foobarspam.mvc;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+
 /**
  *
  * @author Sergi
  */
 @Controller
 public class CarreraServiceController {
+    
 
-    @RequestMapping(path="/SetPickup")
+    @RequestMapping(path = "/SetPickup")
     @ResponseBody
     public String setPickUp() {
-    	return "setpickup";
+        return "setpickup";
+    }
+
+    @RequestMapping(value = "/Bones", method = RequestMethod.GET)
+    @ResponseBody
+    public String bones(HttpServletRequest request, Model model) {
+
+        String nom = request.getParameter("nom");
+
+        if (nom.isEmpty()) {
+            nom = "merda";
+        }
+        model.addAttribute("name", nom);
+        return "Uep, " + nom;
+
     }
 }
