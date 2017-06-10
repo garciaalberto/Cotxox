7949c6099de59e008a464f138dea7e337a2a5829
seergi93 <sergiroberti93@gmail.com>
2017-06-10 15:05:13 +0200

    Parse distancia y tiempo ok,

--- src/main/java/org/foobarspam/mvc/MapsTools.java
+++ src/main/java/org/foobarspam/mvc/MapsTools.java
@@ -8,8 +8,6 @@ package org.foobarspam.mvc;
 import static antlr.Utils.error;
 import java.awt.Dimension;
 import java.awt.Image;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
 import maps.java.*;
 
 /**
@@ -21,7 +19,7 @@ public class MapsTools extends Route {
     private double distanciaTotal;
     private double tiempoTotal;
     private String origen, destino;
-    private StaticMaps ObjStatMap = new StaticMaps();
+    private String UrlMapaDesino;
     private Route ruta = new Route();
 
     public MapsTools() {
@@ -47,25 +45,52 @@ public class MapsTools extends Route {
         this.origen = origen;
         this.destino = destino;
         try {
-            String[][] resultado = this.ruta.getRoute(this.origen, this.destino, null, Boolean.TRUE, Route.mode.driving, Route.avoids.nothing);
 
+            StaticMaps ObjStatMap = new StaticMaps();
+            String[][] resultado = this.ruta.getRoute(this.origen, this.destino, null, Boolean.TRUE, Route.mode.driving, Route.avoids.nothing);
+            Image resultadoMapa = ObjStatMap.getStaticMap(this.destino, 15, new Dimension(1000, 1000),
+                    1, StaticMaps.Format.png, StaticMaps.Maptype.roadmap);
+
+            this.UrlMapaDesino = MapsJava.getLastRequestURL();
         } catch (Exception e) {
             error("Ruta no válida");
         }
 
+        parsearResultado();
+
     }
 
+    private void parsearResultado() {
+        double dTotal = 0;
+        double tTotal = 0;
+
+        for (double distancia : ruta.getTotalDistance()) {
+            dTotal += distancia;
+        }
+
+        this.distanciaTotal = dTotal;
+        for (double tiempo : ruta.getTotalTime()) {
+            tTotal += tiempo;
+        }
+
+        this.tiempoTotal = tTotal;
+
+    }
+
+    //Per si volem sa imatge en local
+    /*
     public void obtenerMapaDestino() {
+
         try {
             Image resultadoMapa = ObjStatMap.getStaticMap(this.destino, 15, new Dimension(1000, 1000),
                     1, StaticMaps.Format.png, StaticMaps.Maptype.roadmap);
-            System.out.println("La URL asociada al mapa es: " + MapsJava.getLastRequestURL());
+            this.UrlMapaDesino = MapsJava.getLastRequestURL();
         } catch (Exception e) {
             error("Mapas estáticos");
         }
 
     }
-
+     */
     public double getDistanciaTotal() {
         return distanciaTotal;
     }
@@ -96,6 +121,10 @@ public class MapsTools extends Route {
 
     public void setRuta(Route ruta) {
         this.ruta = ruta;
+    }
+
+    public String getUrlMapaDesino() {
+        return UrlMapaDesino;
     }
 
 }
