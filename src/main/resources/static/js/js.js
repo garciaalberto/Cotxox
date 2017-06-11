93208fa0512f126f0ee7a8d21d80b2fdc5bb1920
Alberto García <Bertooooo10@gmail.com>
2017-06-11 12:21:45 +0200

    Tercera historia de usuario, requiere cambiar todos los valores fijados por los de un conductor random que no esté ocupado y btstrp

--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -7,3 +7,8 @@ function requestCar(){
         alert("Por favor, introduce un Origen/Destino para la carrera");
     }
 }
+
+function acceptPayment(){
+        document.getElementById("acceptPayment").style.display = "none";
+        document.getElementById("ride").style.display = "block";
+}
