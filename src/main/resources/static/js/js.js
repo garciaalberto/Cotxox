2fa9d43d81535734913512fe80787fe0ba39939e
Alberto García <Bertooooo10@gmail.com>
2017-06-11 11:24:54 +0200

    Primera historia de usuario super cutre y sin Bootstrap pero funcional

--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -1,7 +1,8 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-
+function requestCar(){
+    if (document.getElementById("origen").value !== "" && document.getElementById("destino").value !== ""){
+        document.getElementById("requestcar").style.display = "none";
+    } else {
+        alert("Por favor, introduce un Origen/Destino para la carrera");
+    }
+}
