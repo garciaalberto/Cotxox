cee5af56a2ea88ab6f95146e1788d14586bf1010
Alberto García <Bertooooo10@gmail.com>
2017-06-14 15:15:33 +0200

    Small tweak

--- src/main/java/org/foobarspam/mvc/Tarifa.java
+++ src/main/java/org/foobarspam/mvc/Tarifa.java
@@ -4,54 +4,54 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Tarifa{
 	
-	private double tiempo = 0d;
-	private double distancia = 0d;
-	private final double minimo = 5.0;
-	private final double costeTiempo = 0.35;
-	private final double costeMilla = 1.35;
-	
-	Tarifa(){
-		
-	}
-	
-	Tarifa(double distancia, double tiempo){
-		this.tiempo = tiempo;
-		this.distancia = distancia;
-	}
-	
-	
-	
-	public double getTiempo() {
-		return this.tiempo;
-	}
+    private double tiempo = 0d;
+    private double distancia = 0d;
+    private final double minimo = 5.0;
+    private final double costeTiempo = 0.35;
+    private final double costeMilla = 1.35;
 
-	public void setTiempo(double tiempo) {
-		this.tiempo = tiempo;
-	}
+    Tarifa(){
 
-	public double getDistancia() {
-		return this.distancia;
-	}
+    }
 
-	public void setDistancia(double distancia) {
-		this.distancia = distancia;
-	}
+    Tarifa(double distancia, double tiempo){
+            this.tiempo = tiempo;
+            this.distancia = distancia;
+    }
 
-	public double getMinimo() {
-		return this.minimo;
-	}
 
-	public double getCosteTiempo() {
-		return this.costeTiempo;
-	}
 
-	public double getCosteMilla() {
-		return this.costeMilla;
-	}
+    public double getTiempo() {
+            return this.tiempo;
+    }
 
-	public double calculatePrecio(double distancia, double tiempo) {
-        tiempo = tiempo * getCosteTiempo();
-        distancia = distancia * getCosteMilla();
+    public void setTiempo(double tiempo) {
+            this.tiempo = tiempo;
+    }
+
+    public double getDistancia() {
+            return this.distancia;
+    }
+
+    public void setDistancia(double distancia) {
+            this.distancia = distancia;
+    }
+
+    public double getMinimo() {
+            return this.minimo;
+    }
+
+    public double getCosteTiempo() {
+            return this.costeTiempo;
+    }
+
+    public double getCosteMilla() {
+            return this.costeMilla;
+    }
+
+    public double calculatePrecio(double distancia, double tiempo) {
+    tiempo = tiempo * getCosteTiempo();
+    distancia = distancia * getCosteMilla();
         if ((tiempo + distancia) > getMinimo()) {
             return Redondear(tiempo + distancia);
         } else {
@@ -59,14 +59,14 @@ public class Tarifa{
         }
     }
         
-        public double Redondear(double numero)
-        {
-              return Math.rint(numero*100)/100;
-        }
-	
-	public double randomizeDistance() {
-        double distance = ThreadLocalRandom.current().nextInt(0, 30);
-        return distance;
+    public double Redondear(double numero)
+    {
+          return Math.rint(numero*100)/100;
+    }
+
+    public double randomizeDistance() {
+    double distance = ThreadLocalRandom.current().nextInt(0, 30);
+    return distance;
     }
 
     public double randomizeTime() {
--- src/main/java/org/foobarspam/repository/CarreraService.java
+++
@@ -1,6 +0,0 @@
-
-package org.foobarspam.repository;
-
-public class CarreraService {
-    
-}
